ab9982631cd115331826a347797c4e3969dc1dff
Twan Bolwerk <t.bolwerk@student.science.ru.nl>
2022-02-11 12:10:08 +0100

    tsp

--- EC/p6/p6a.py
+++ EC/p6/p6a.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 
 
 
@@ -7,7 +8,7 @@ import itertools
 # TODO: Crossover, mutation for 6a this is a simple EA algorithm so we choose (1+1)-EC
 # 
 
-coords = [[1,2], [3,4], [2,3], [4,4]]
+coords = [[1,2], [3,4], [2,3], [4,4], (7,7), (10,5), (4,2)]
 goal = 1
 def distance (src, dest):
    x = abs(src[0] - dest[0])
@@ -36,26 +37,32 @@ def intersection(lst1, lst2):
 def crossover(cutpoint_start, cutpoint_end, parent1, parent2):
     parent1_schema = [ x for i, x in enumerate(parent1) if i < cutpoint_start or i > cutpoint_end]
     parent2_schema = [ x for i, x in enumerate(parent2) if i < cutpoint_start or i > cutpoint_end]
-    k = []
-    k.extend(parent1_schema)
-    k.extend(parent2_schema) # create list of possible mutation crossover
-    new_k = []
-    for elem in k: # remove dups
-        if elem not in new_k:
-           new_k.append(elem)
-    perms = list(itertools.permutations(new_k))
+    k1 = []
+    k2 = []
+    k1.extend(parent1_schema)
+    k2.extend(parent2_schema) # create list of possible mutation crossover
+    new_k1 = []
+    new_k2 = []
+    for elem in k1: # remove dups
+        if elem not in new_k1:
+           new_k1.append(elem)
+    for elem in k2: # remove dups
+        if elem not in new_k2:
+           new_k2.append(elem)
+    perms1 = list(itertools.permutations(new_k1))
+    perms2 = list(itertools.permutations(new_k2))
     offspring1 = list(parent1)
     offspring2 = list(parent2)
     mutation_counter = cutpoint_end - cutpoint_start - 1
-    x = 2 # random permutation selection
-    y = 3 # random permuation selection
+    x = random.randint(0, len(new_k1)-1) # random permutation selection
+    y = random.randint(0, len(new_k2)-1) # random permuation selection
     print(repr(parent1_schema))
     for i in range(len(offspring1)):
         if i < cutpoint_start or i > cutpoint_end:
-            if list(perms[x][mutation_counter]) not in intersection(offspring1, parent1_schema): 
-               offspring1[i] = list(perms[x])[mutation_counter]
-            if list(perms[y][mutation_counter]) not in intersection(offspring2, parent2_schema): 
-               offspring2[i] = list(perms[y])[mutation_counter]
+            if list(perms1[x][mutation_counter]) not in intersection(offspring1, parent1_schema): 
+               offspring1[i] = list(perms1[x])[mutation_counter]
+            if list(perms2[y][mutation_counter]) not in intersection(offspring2, parent2_schema): 
+               offspring2[i] = list(perms2[y])[mutation_counter]
             mutation_counter = mutation_counter - 1
     return offspring1, offspring2
 
@@ -97,12 +104,15 @@ def tsp():
     parent1 = perms[0]
     parent2 = perms[1]
     
+    count = 5
+
     fst, snd = crossover(cutpoint_start, cutpoint_end, perms[0], perms[1]) 
-    print_info(parent1,parent2, fst, snd)
-    parent1 = fst
-    parent2 = snd
-    fst, snd = crossover(cutpoint_start, cutpoint_end, perms[0], perms[1]) 
-    print_info(parent1,parent2, fst, snd)
+    while(count > 0):
+        count = count - 1
+        print_info(parent1,parent2, fst, snd)
+        fst, snd = crossover(cutpoint_start, cutpoint_end, perms[0], perms[1]) 
+        parent1 = fst
+        parent2 = snd
 
 
 tsp()
